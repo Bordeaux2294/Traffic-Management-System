0a624fd51a9bb69f037934e4a3dceb511ef9794e
juneplum <119058517+juneplum@users.noreply.github.com>
2024-05-12 22:37:30 -0500

    added function call

--- wrong_turn_fin.py
+++ wrong_turn_fin.py
@@ -7,27 +7,27 @@ Original file is located at
     https://colab.research.google.com/drive/1UhqqgvW-Y88617jjIpVONUqw6Tz0fxAa
 """
 
-!pip install torch torchvision -U
-!pip install ultralytics
-!pip install supervision
-!pip install cvzone
-!pip install cv2
+# !pip install torch torchvision -U
+# !pip install ultralytics
+# !pip install supervision
+# pip install cvzone
+# pip install cv2
 
 #get coordinates for region of interest
 #from google.colab.patches import cv2_imshow
-def Capture_Event(event, x, y, flags, params):
-	#left mouse button is pressed
-	if event == cv2.EVENT_LBUTTONDOWN:
-		#print x, y coordinates to screen
-		print(f"({x}, {y})")
+# def Capture_Event(event, x, y, flags, params):
+# 	#left mouse button is pressed
+# 	if event == cv2.EVENT_LBUTTONDOWN:
+# 		#print x, y coordinates to screen
+# 		print(f"({x}, {y})")
 
-if __name__=="__main__":
-	img = cv2.imread('firstframe.jpg', 1)
-	cv2_imshow(img)
-	cv2.setMouseCallback('image', Capture_Event)
-	# Press any key to exit
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+# if __name__=="__main__":
+# 	img = cv2.imread('firstframe.jpg', 1)
+# 	cv2_imshow(img)
+# 	cv2.setMouseCallback('image', Capture_Event)
+# 	# Press any key to exit
+# 	cv2.waitKey(0)
+# 	cv2.destroyAllWindows()
 
 # import cv2
 # #get first frame of video
@@ -57,113 +57,170 @@ if __name__=="__main__":
 # cap.release()
 # cv2.destroyAllWindows()
 
-from google.colab.patches import cv2_imshow
+#from google.colab.patches import cv2_imshow
 
-import cv2
-from tracker import*
-import cvzone
-import numpy as np
-import os
-from datetime import datetime
-# Initialize the video capture, saving output to a video
-cap = cv2.VideoCapture('wrongway.mp4')
-output_video_path = 'wrongturn.mp4'
-frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = int(cap.get(cv2.CAP_PROP_FPS))
-fourcc = cv2.VideoWriter_fourcc(*'XVID')  #define the codec
-out = cv2.VideoWriter(output_video_path, fourcc, fps, (frame_width, frame_height))
+import math
 
-#create a background subtractor
-fgbg = cv2.createBackgroundSubtractorMOG2()
+def detect_turn(video_path, output_video_path):
+    class Tracker:
+        def __init__(self):
+            # Store the center positions of the objects
+            self.center_points = {}
+            # Keep the count of the IDs
+            # each time a new object id detected, the count will increase by one
+            self.id_count = 0
 
-# area1=[(593,227),(602,279),(785,274),(774,220)]
-# area2=[(747,92),(785,208),(823,202),(773,95)]
-area1=[(71, 212),(329, 247),(324, 270),(31, 236)]
-area2=[(280, 240),(322, 141),(357, 146),(319, 251)]
-tracker=Tracker()
-#save id and position of vehicles
-a1={}
-counter=[]
 
-#save image of wrong turn with current date and time
-def save_full_frame(frame):
-    # Create a folder with the current date and time as the folder name
-    current_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
-    folder_name = f"wrongturn"
-    os.makedirs(folder_name, exist_ok=True)
+        def update(self, objects_rect):
+            # Objects boxes and ids
+            objects_bbs_ids = []
 
-    # Save the entire frame
-    image_filename = os.path.join(folder_name, f"frame_{current_datetime}.jpg")
-    cv2.imwrite(image_filename, frame)
-while True:
-    ret, frame = cap.read()
+            # Get center point of new object
+            for rect in objects_rect:
+                x, y, w, h = rect
+                cx = (x + x + w) // 2
+                cy = (y + y + h) // 2
 
-    if not ret:
-        break
-    #frame=cv2.resize(frame,(1020,500))
-    frame=cv2.resize(frame,(450,360))
+                # Find out if that object was detected already
+                same_object_detected = False
+                for id, pt in self.center_points.items():
+                    dist = math.hypot(cx - pt[0], cy - pt[1])
 
-    # Apply background subtraction
-    #fgbg represents clear bg, subtracting the background
-    fgmask = fgbg.apply(frame)
+                    if dist < 35:
+                        self.center_points[id] = (cx, cy)
+                        print(self.center_points) #
+                        objects_bbs_ids.append([x, y, w, h, id])
+                        same_object_detected = True
+                        break
 
-    #threshold the foreground mask
-    #250 255 coordinates
-    _, thresh = cv2.threshold(fgmask, 250, 250, cv2.THRESH_BINARY)
+                # New object is detected we assign the ID to that object
+                if same_object_detected is False:
+                    self.center_points[self.id_count] = (cx, cy)
+                    objects_bbs_ids.append([x, y, w, h, self.id_count])
+                    self.id_count += 1
 
-    # Find contours in the thresholded image
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # Clean the dictionary by center points to remove IDS not used anymore
+            new_center_points = {}
+            for obj_bb_id in objects_bbs_ids:
+                _, _, _, _, object_id = obj_bb_id
+                center = self.center_points[object_id]
+                new_center_points[object_id] = center
 
-    # Draw rectangles around moving objects
-    list=[]
-    for contour in contours:
-        if cv2.contourArea(contour) >=7:  # Adjust the area threshold as needed
-            x, y, w, h = cv2.boundingRect(contour)
-            list.append([x,y,w,h])
-    bbox_idx=tracker.update(list)
-    for bbox in bbox_idx:
-        x1,y1,w1,h1,id=bbox
-        cx=int(x1+x1+w1)//2
-        cy=int(y1+y1+h1)//2
-        result=cv2.pointPolygonTest(np.array(area1,np.int32),((cx,cy)),False)
-        if result>=0:
-           a1[id]=(cx,cy)
-        if id in a1:
-           result1=cv2.pointPolygonTest(np.array(area2,np.int32),((cx,cy)),False)
-           if result1>=0:
-              cv2.rectangle(frame, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0), 2)
-              cv2.circle(frame,(cx,cy),7,(0,255,0),-1)
-              cvzone.putTextRect(frame,f'{id}',(x1,y1),1,1)
-              if counter.count(id)==0:
-                 counter.append(id)
-                 save_full_frame(frame)
-#draw region of interest on frame
+            # Update dictionary with IDs not used removed
+            self.center_points = new_center_points.copy()
+            return objects_bbs_ids
 
-    cv2.polylines(frame,[np.array(area1,np.int32)],True,(0,255,0),2)
-    cv2.polylines(frame,[np.array(area2,np.int32)],True,(0,0,255),2)
+    import cv2
+    #from tracker import*
+    import cvzone
+    import numpy as np
+    import os
+    from datetime import datetime
 
-    p=len(counter)
-#add text with counter to screen
-    cvzone.putTextRect(frame,f'Wrong turns: {p}',(10,50),1,2,
-        colorT=(255,255, 255), colorR=(1, 1, 1),
-        font=cv2.FONT_HERSHEY_SIMPLEX,
-        offset=10,
-        border=2, colorB=(255, 255, 255) )
-    out.write(frame)
-    cv2_imshow(frame)
+    #def detect_turn(video_path, output_video_path, model_path='best.pt', class_list_path='coco1.txt'):
+    # Initialize the video capture, saving output to a video
+    cap = cv2.VideoCapture('wrongturns.mp4')
+    #creating folder to save output to folder
+    output_video_path = 'wrongway.mp4'
+    output_folder = "violations"
+    os.makedirs(output_folder, exist_ok=True)  # Create the "violations" folder if it doesn't exist
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    output_video_name = f"wrong_way_{timestamp}.mp4"
+    output_video_path = os.path.abspath(os.path.join(output_folder, output_video_name))
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')  #define the codec
+    out = cv2.VideoWriter(output_video_path, fourcc, fps, (frame_width, frame_height))
 
-    if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' to exit
-        break
+    #create a background subtractor
+    fgbg = cv2.createBackgroundSubtractorMOG2()
 
-# Release the video capture and close all OpenCV windows
-cap.release()
-cv2.destroyAllWindows()
-out.release()
+    # area1=[(593,227),(602,279),(785,274),(774,220)]
+    # area2=[(747,92),(785,208),(823,202),(773,95)]
+    area1=[(71, 212),(329, 247),(324, 270),(31, 236)]
+    area2=[(280, 240),(322, 141),(357, 146),(319, 251)]
+    tracker=Tracker()
+    #save id and position of vehicles
+    a1={}
+    counter=[]
 
-print("Video saved successfully!")
+    while True:
+        ret, frame = cap.read()
 
-#first vid
-# area1=[(71, 212),(329, 247),(324, 270),(31, 236)]
-# area2=[(280, 240),(322, 141),(357, 146),(319, 251)]
-# if cv2.contourArea(contour) >= 7:
+        if not ret:
+            break
+        #frame=cv2.resize(frame,(1020,500))
+        frame=cv2.resize(frame,(450,360))
+
+        # Apply background subtraction
+        #fgbg represents clear bg, subtracting the background
+        fgmask = fgbg.apply(frame)
+
+        #threshold the foreground mask
+        #250 255 coordinates
+        _, thresh = cv2.threshold(fgmask, 250, 250, cv2.THRESH_BINARY)
+
+        # Find contours in the thresholded image
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Draw rectangles around moving objects
+        list=[]
+        for contour in contours:
+            if cv2.contourArea(contour) >=7:  # Adjust the area threshold as needed
+                x, y, w, h = cv2.boundingRect(contour)
+                list.append([x,y,w,h])
+        bbox_idx=tracker.update(list)
+        for bbox in bbox_idx:
+            x1,y1,w1,h1,id=bbox
+            cx=int(x1+x1+w1)//2
+            cy=int(y1+y1+h1)//2
+            result=cv2.pointPolygonTest(np.array(area1,np.int32),((cx,cy)),False)
+            if result>=0:
+                a1[id]=(cx,cy)
+            if id in a1:
+                result1=cv2.pointPolygonTest(np.array(area2,np.int32),((cx,cy)),False)
+                if result1>=0:
+                    cv2.rectangle(frame, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0), 2)
+                    cv2.circle(frame,(cx,cy),7,(0,255,0),-1)
+                    cvzone.putTextRect(frame,f'{id}',(x1,y1),1,1)
+                    if counter.count(id)==0:
+                        counter.append(id)
+                        #save_full_frame(frame)
+    #draw region of interest on frame
+
+        cv2.polylines(frame,[np.array(area1,np.int32)],True,(0,255,0),2)
+        cv2.polylines(frame,[np.array(area2,np.int32)],True,(0,0,255),2)
+
+        p=len(counter)
+    #add text with counter to screen
+        cvzone.putTextRect(frame,f'Wrong turns: {p}',(10,50),1,2,
+            colorT=(255,255, 255), colorR=(1, 1, 1),
+            font=cv2.FONT_HERSHEY_SIMPLEX,
+            offset=10,
+            border=2, colorB=(255, 255, 255))
+       
+        out.write(frame)
+        cv2.imshow("frame",frame)
+        
+        if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' to exit
+            break
+        
+
+    
+    # Release the video capture and close all OpenCV windows
+    cap.release()
+    cv2.destroyAllWindows()
+    out.release()
+    
+    info_list = ["wrongturns.mp4", "illegal_turn", "Kingston", datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "Users\tntdr\Downloads\turn_videos\violations"]
+    return info_list
+    print("Video saved successfully!")
+
+result_info = detect_turn('wrongturns.mp4', 'wrongway.mp4')
+print(result_info)
+        #first vid
+        # area1=[(71, 212),(329, 247),(324, 270),(31, 236)]
+        # area2=[(280, 240),(322, 141),(357, 146),(319, 251)]
+        # if cv2.contourArea(contour) >= 7:
+    #detect_turn('wrongway.mp4','wrongway.mp4')
