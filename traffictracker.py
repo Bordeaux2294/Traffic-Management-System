7933a88820a69e9986a8cecfbbbe5cfe973e29ba
RLampart <rockingriches@gmail.com>
2024-05-13 21:12:01 -0500

    limit emails

--- traffictracker.py
+++ traffictracker.py
@@ -2,21 +2,29 @@ from ultralytics import YOLO
 from email.mime.text import MIMEText
 import smtplib
 import numpy as np
+from datetime import datetime
+from database import SQLHandler
 
 class TrafficTracker():
     def __init__(self):
         self.model = YOLO('yolov8n.pt')
+        self.connection = SQLHandler()
         self.dens_list = []
+        self.cls = 'safe'
         self.sender = "TekSupport <software@teksupport.com>"
         self.receiver = "National Works Agency <ccs@nwa.gov.jm>"
 
     def eval(self):
         if np.min(self.dens_list) >= 1:
             msg = f"Traffic at {self.location} is at a critical level"
-            self.send(msg)
+            if self.cls != "critical":
+               self.cls = "critical"
+               self.send(msg)
         elif np.min(self.dens_list) >= 0.5:
             msg = f"Traffic at {self.location} is at a high level"
-            self.send(msg)
+            if self.cls != "high":
+               self.cls = "high"
+               self.send(msg)
         self.dens_list = self.dens_list[1:]
 
 
@@ -30,6 +38,9 @@ class TrafficTracker():
         area = ((y[1]-y[0])/200) * ((x[1]-x[0])/200)
         vehicles = len(result[0].boxes.cls)
         dens = vehicles/area
+        place = location.split()
+        vals = [dens,self.cls,("").join(place[:-1]),place[-1],datetime.now()]
+        self.connection.add_density(vals)
         self.dens_list.append(dens)
         if len(self.dens_list) >= 5:
            self.eval()
